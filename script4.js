6552d3eef35a8a4bdac519e121aff80585188fc6
joris <97834949+Georgyporgie@users.noreply.github.com>
2024-05-11 21:46:45 +0200

    Add files via upload

---
+++ script4.js
@@ -0,0 +1,81 @@
+
+
+
+const container = document.getElementById('container');
+const text = document.getElementById('text');
+const totalTime = 7500;
+const breatheTime = (totalTime / 5) * 2;
+const holdTime = totalTime / 5;
+
+
+breathAnimation();
+
+function breathAnimation() {
+text.innerText = 'Breath In!';
+container.className = 'container grow';
+
+
+setTimeout(() =>  {
+text.innerText = 'Hold';
+
+setTimeout(() => {
+text.innerText = 'Breath Out!';
+container.className = 'container shrink';  
+}, holdTime);  
+}, breatheTime);
+}
+setInterval(breathAnimation, totalTime);
+
+
+
+
+
+
+var Timer = function(opts) {
+  var self = this;
+
+  self.opts     = opts || {};
+  self.element  = opts.element || null;
+  self.minutes  = opts.minutes || 0;
+  self.seconds  = opts.seconds || 30;
+
+  self.start = function() {
+    self.interval = setInterval(countDown, 1000);
+  };
+
+  self.stop = function() {
+    clearInterval(self.interval);
+  };
+
+  function countDown() {
+    self.seconds--;
+    if (self.minutes == 0 && self.seconds == 0) {
+      self.stop();window.location.href = "adem.html";
+    }
+
+    if (self.seconds < 0) {
+      self.seconds = 59;
+      self.minutes--;
+    }
+
+    if (self.seconds <= 9) { self.seconds = '0' + self.seconds; }
+
+    self.element.textContent = self.minutes + ' : ' + self.seconds;
+  }
+};
+
+var myTimer = new Timer({
+  minutes: 4,
+  seconds: 59,
+  element: document.querySelector('#timer')
+});
+
+myTimer.start();
+
+
+
+
+
+
+
+
