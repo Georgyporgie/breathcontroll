7a7cf1fe2458c01e831da82048cab2e46f1c6b8e
joris <97834949+Georgyporgie@users.noreply.github.com>
2024-05-11 22:00:15 +0200

    Add files via upload

---
+++ script3.js
@@ -0,0 +1,30 @@
+const container = document.getElementById('container');
+const text = document.getElementById('text');
+const totalTime = 1700;
+const breatheTime = (totalTime / 15) * 6;
+const holdTime = totalTime / 15;
+
+
+breathAnimation();
+
+function breathAnimation() {
+text.innerText = 'Press Start';
+container.className = 'container grow';
+
+
+setTimeout(() =>  {
+text.innerText = '';
+
+setTimeout(() => {
+text.innerText = '';
+container.className = 'container shrink';  
+}, holdTime);  
+}, breatheTime);
+}
+setInterval(breathAnimation, totalTime);
+
+
+
+function start()  {
+window.location.href = "index.html";
+}
